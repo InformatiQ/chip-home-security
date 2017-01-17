e6e87597a7ae58879a1c4b5d0dcfc5eec010b675
Ramez Hanna <r.hanna@criteo.com>
2017-01-17 09:04:17 +0100

    threading works

--- alarm.py
+++ alarm.py
@@ -1,37 +1,68 @@
-import os, time
-import CHIP_IO.GPIO as GPIO
+import os, sys, time, signal
+#import CHIP_IO.GPIO as GPIO
 import threading
 
 class MotionDetect(threading.Thread):
     def __init__(self, pin):
-        GPIO.setup(pin, GPIO.IN)
+        #GPIO.setup(pin, GPIO.IN)
+        super(MotionDetect, self).__init__()
+        self._stop = threading.Event()
 
-    def detect(self):
-        global Detected
-        if GPIO.input("XIO-P0"):
-            Detected = True
+    def run(self):
+        print("Detecting motion"),
+        while not self._stop.isSet():
+            print "thread stop: %s" %self._stop.isSet()
+            print("."),
+            global Detected
+            #if GPIO.input("XIO-P0"):
+            #    Detected = True
+            if os.path.exists('/tmp/motion'):
+                if not Detected:
+                    print "motion detected"
+                Detected = True
+            time.sleep(2)
 
     def reset(self):
         global Detected
         Detected = False
 
-GPIO.setup("XIO-P1", GPIO.OUT)
+    def stop(self):
+        print "terminating thread"
+        self._stop.set()
+
+#GPIO.setup("XIO-P1", GPIO.OUT)
 def alert():
     print "ON"
-    GPIO.output("XIO-P1", 0)
+    #GPIO.output("XIO-P1", 0)
 
 def reset():
     print "OFF"
-    GPIO.output("XIO-P1", 1)
+    #GPIO.output("XIO-P1", 1)
 
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    motion.stop()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
-if __name__ == "__main__":
+def main():
     try:
         Detected = False
-        motion = MotionDetect('XIO-P0')
-        motion.detect()
-        if Detected:
-            alert
+        motion = MotionDetect('foo')
+        print "start detection"
+        motion.start()
+        while True:
+            print "checking %s" %Detected
+            if Detected:
+                alert()
+            if os.path.exists('/tmp/reset'):
+                Detected = False
+                reset()
+            time.sleep(2)
+    except Exception as e:
+        print "error -> %s" %str(e)
+        motion.stop()
+        #GPIO.cleanup()
 
-    except:
-        GPIO.cleanup()
+if __name__ == "__main__":
+    main()
